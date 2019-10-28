5569978a720ed14e77192377ba88983ddba5d8e8
Taliaa <taliaa10@ymail.com>
2019-10-28 13:49:57 -0500

    reorg

---
+++ Inheritence/access-specifiers.py
@@ -0,0 +1,20 @@
+# Public => memberName
+# Protected => _memberName
+# Private => __Private
+
+
+class Car:
+    numberOfWheels = 4
+    _color = 'Black'
+    __yearOfManufacture = 2017  # _Car__yearOfManufacture
+
+
+class BMW(Car):
+    def __init__(self):
+        print('Protected attribute color:', self._color)
+
+
+car = Car()
+print('Public attribute numberOfWheels', car.numberOfWheels)
+bmw = BMW()
+print('Private attribute yearOfManufacture:', car._Car__yearOfManufacture)
